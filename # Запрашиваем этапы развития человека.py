e553bb72e52b21b20c71c9f7d6cab0513f8012f8
moonoon <81164166+moonriseE@users.noreply.github.com>
2025-11-18 10:11:41 +0500

    Add files via upload

---
+++ # Запрашиваем этапы развития человека.py
@@ -0,0 +1,8 @@
+# Запрашиваем этапы развития человека
+stage1 = input("Введите первую стадию развития (Homo habilis): ")
+stage2 = input("Введите вторую стадию развития (Homo erectus): ")
+stage3 = input("Введите третью стадию развития (Homo neanderthalensis): ")
+stage4 = input("Введите последнюю стадию развития (Homo sapiens sapiens): ")
+
+# Выводим все стадии с разделителем =>
+print(stage1, stage2, stage3, stage4, sep=' => ')
